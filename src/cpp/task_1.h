0c3aafc2be07fc5baced3cdb1c982a0314f4fc74
Itcoderbek <128024527+Itcoderbek@users.noreply.github.com>
2023-10-20 16:15:16 +0500

    Update task_1.h
    commit1

--- src/cpp/task_1.h
+++ src/cpp/task_1.h
@@ -1,5 +1,21 @@
 /*
- * Author:
- * Date:
- * Name:
+ * Author: Diyorbek
+ * Date:10.20.2023
+ * Name:task1
  */
+#ifndef TASK_1_H
+#define TASK_1_H
+
+bool isLeapYear(int year) {
+    if (year % 4 != 0) {
+        return false;
+    } else if (year % 100 != 0) {
+        return true;
+    } else if (year % 400 != 0) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+#endif // TASK_1_H
